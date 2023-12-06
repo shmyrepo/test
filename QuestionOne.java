a2396bb507fd31fbe3801d9d1bf434ed7c54e4f2
shmyrepo <38337248+shmyrepo@users.noreply.github.com>
2023-12-06 11:21:15 -0500

    Create QuestionOne.java

---
+++ QuestionOne.java
@@ -0,0 +1,25 @@
+//Write a Java program that checks if two arrays contain the same elements. Please take a look at the test program to understand the expected results.
+
+import java.util.Arrays;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class Solution {
+  boolean sameElements(Integer[] array1, Integer[] array2) {
+    //TODO: Implement your solution here
+  }
+
+  @Test
+  void testYourProgram() {
+    Integer[] a1 = {1,2,3,2,1};
+    Integer[] a2 = {1,2,3};
+    Integer[] a3 = {1,2,3,4};
+
+    Assert.assertTrue(sameElements(a1,a2));
+    Assert.assertTrue(sameElements(a2,a1));
+    Assert.assertFalse(sameElements(a1,a3));
+    Assert.assertFalse(sameElements(a3,a2));
+
+    //Add More Cases Here
+  }
+}
